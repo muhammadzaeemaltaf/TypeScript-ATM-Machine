0c8735ccae1185aa5026588192f7cda693cd49b9
muhammadzaeemaltaf <165184980+muhammadzaeemaltaf@users.noreply.github.com>
2024-04-11 13:32:09 +0500

    update

--- main.js
+++ main.js
@@ -1,50 +1,84 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-let balance = 10000; //Dollor
-let annountNo = 123456;
-let pinCode = 1006;
+const balance = 10000; //Dollor
+const pinCode = 1006;
 let pinAnswer = await inquirer.prompt([
     {
         name: "q1",
-        message: "Enter your account no: ",
-        type: "number"
-    },
-    {
-        name: "q2",
         message: "Enter your pin: ",
-        type: "number"
-    }
+        type: "number",
+    },
 ]);
-if (pinAnswer.q1 === annountNo && pinAnswer.q2 === pinCode) {
+if (pinAnswer.q1 === pinCode) {
     let userName = "Zaeem Altaf";
-    console.log(`Welcome ${userName}! Your current balance is $${balance}`);
+    console.log(`\r\n Welcome ${userName}! Your current balance is $${balance} \r\n`);
     let operationAns = await inquirer.prompt([
         {
             name: "operation",
             message: "Please Select Options",
             type: "list",
-            choices: ["withdraw", "Check balance"]
-        }
+            choices: [
+                "Cash withdrawl",
+                "Fast Cash",
+                "Fund Transfer",
+                "Check balance",
+            ],
+        },
     ]);
-    if (operationAns.operation === "withdraw") {
+    if (operationAns.operation === "Cash withdrawl") {
         let amount = await inquirer.prompt([
             {
                 name: "amount",
-                message: "How much money do you want to withdraw?",
-                type: "number"
-            }
+                message: "How much money do you want to withdrawl?",
+                type: "number",
+            },
         ]);
         if (amount.amount > balance) {
             console.log("Insufficient balance");
         }
         else {
-            console.log(`Successfully  withdraw $${amount.amount}.`);
+            console.log(`Successfully  withdrawl $${amount.amount}.`);
             let remainingBalance = balance - amount.amount;
             console.log(`Your remaning balance is $${remainingBalance}.`);
         }
     }
-    else if (operationAns.operation === "Check balance") {
-        console.log(`Your balance is $${balance}.`);
+    else if (operationAns.operation === "Fast Cash") {
+        let fastCash = await inquirer.prompt([
+            {
+                name: "cashAmount",
+                type: "list",
+                message: "How much money do you want to withdrawl?",
+                choices: [2000, 4000, 8000],
+            },
+        ]);
+        if (fastCash.cashAmount > balance) {
+            console.log("Insufficient balance");
+        }
+        else {
+            console.log(`Successfully  withdrawl $${fastCash.cashAmount}.`);
+            let remainingBalance = balance - fastCash.cashAmount;
+            console.log(`Your remaning balance is $${remainingBalance}.`);
+        }
+    }
+    else if (operationAns.operation === "Fund Transfer") {
+        let fund = await inquirer.prompt([
+            {
+                name: "fundAmount",
+                type: "number",
+                message: "How much money do you want to transfer?",
+            },
+        ]);
+        if (fund.fundAmount > balance) {
+            console.log("Insufficient balance");
+        }
+        else {
+            console.log(`Successfully  transfer $${fund.fundAmount} to John Doe.`);
+            let remainingBalance = balance - fund.fundAmount;
+            console.log(`Your remaning balance is $${remainingBalance}.`);
+        }
+    }
+    else {
+        console.log(`Your current balance is $${balance}.`);
     }
 }
 else {
--- main.ts
+++ main.ts
@@ -2,55 +2,85 @@
 
 import inquirer from "inquirer";
 
-let balance = 10000; //Dollor
-let annountNo = 123456;
-let pinCode = 1006;
+const balance = 10000; //Dollor
+const pinCode = 1006;
 
 let pinAnswer = await inquirer.prompt([
-   {
+  {
     name: "q1",
-    message: "Enter your account no: ",
-    type: "number"
-   },
-    {
-        name: "q2",
-        message: "Enter your pin: ",
-        type: "number"
-    }
+    message: "Enter your pin: ",
+    type: "number",
+  },
 ]);
 
-if (pinAnswer.q1 === annountNo && pinAnswer.q2 === pinCode) {
-    let userName = "Zaeem Altaf";
-    console.log(`Welcome ${userName}! Your current balance is $${balance}`);
-    
-    let operationAns = await inquirer.prompt([
-        {
-            name: "operation",
-            message: "Please Select Options",
-            type: "list",
-            choices: ["withdraw", "Check balance"]
-        }
+if (pinAnswer.q1 === pinCode) {
+  let userName = "Zaeem Altaf";
+  console.log(`\r\n Welcome ${userName}! Your current balance is $${balance} \r\n`);
+
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "Please Select Options",
+      type: "list",
+      choices: [
+        "Cash withdrawl",
+        "Fast Cash",
+        "Fund Transfer",
+        "Check balance",
+      ],
+    },
+  ]);
+
+  if (operationAns.operation === "Cash withdrawl") {
+    let amount = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "How much money do you want to withdrawl?",
+        type: "number",
+      },
     ]);
 
-    if(operationAns.operation === "withdraw"){
-        let amount = await inquirer.prompt([
-            {
-                name: "amount",
-                message: "How much money do you want to withdraw?",
-                type: "number"
-            }
-        ]);
-     
-        if (amount.amount > balance) {
-            console.log("Insufficient balance");
-        }else{
-            console.log(`Successfully  withdraw $${amount.amount}.`);
-            let remainingBalance = balance - amount.amount;
-            console.log(`Your remaning balance is $${remainingBalance}.`);
-        }
-    }else if(operationAns.operation === "Check balance"){
-        console.log(`Your balance is $${balance}.`);
+    if (amount.amount > balance) {
+      console.log("Insufficient balance");
+    } else {
+      console.log(`Successfully  withdrawl $${amount.amount}.`);
+      let remainingBalance = balance - amount.amount;
+      console.log(`Your remaning balance is $${remainingBalance}.`);
     }
-}else {
-    console.log(`Wrong pincode or account number. Please try again.`);
+  } else if (operationAns.operation === "Fast Cash") {
+    let fastCash = await inquirer.prompt([
+      {
+        name: "cashAmount",
+        type: "list",
+        message: "How much money do you want to withdrawl?",
+        choices: [2000, 4000, 8000],
+      },
+    ]);
+    if (fastCash.cashAmount > balance) {
+      console.log("Insufficient balance");
+    } else {
+      console.log(`Successfully  withdrawl $${fastCash.cashAmount}.`);
+      let remainingBalance = balance - fastCash.cashAmount;
+      console.log(`Your remaning balance is $${remainingBalance}.`);
+    }
+  } else if (operationAns.operation === "Fund Transfer") {
+    let fund = await inquirer.prompt([
+      {
+        name: "fundAmount",
+        type: "number",
+        message: "How much money do you want to transfer?",
+      },
+    ]);
+    if (fund.fundAmount > balance) {
+      console.log("Insufficient balance");
+    } else {
+      console.log(`Successfully  transfer $${fund.fundAmount} to John Doe.`);
+      let remainingBalance = balance - fund.fundAmount;
+      console.log(`Your remaning balance is $${remainingBalance}.`);
+    }
+  } else {
+    console.log(`Your current balance is $${balance}.`);
+  }
+} else {
+  console.log(`Wrong pincode or account number. Please try again.`);
 }
